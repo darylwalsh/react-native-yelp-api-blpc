98fbfd1724783e25395f7d334e0b109986950720
Daryl <daryl@blpc.us>
2019-08-15 04:30:04 -0400

    fix(typescript_navigation): fix typescript and navigation
    updated navigation to withNavigation and added TS typings

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation'
 
+import ResultsShowScreen from './src/screens/ResultsShowScreen'
 import SearchScreen from './src/screens/SearchScreen'
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
+    ResultsShow: ResultsShowScreen,
   },
   {
     initialRouteName: 'Search',
--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -1,6 +1,14 @@
 import { string } from 'prop-types'
 import React from 'react'
 import { FlatList, FlatListProps, StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import {
+  NavigationInjectedProps,
+  NavigationScreenComponent,
+  NavigationScreenProps,
+  NavigationStackScreenOptions,
+  withNavigation,
+} from 'react-navigation'
 
 import ResultsDetail from './ResultsDetail'
 
@@ -9,20 +17,45 @@ import ResultsDetail from './ResultsDetail'
 //   name: string
 //   length: string
 // }
-const ResultsList = ({ title, results }: { title: string; results: any }) => {
+export interface ResultsPropsInterface {
+  // navigation: NavigationScreenProp<NavigationState, NavigationParams>
+  navigation: NavigationStackScreenOptions
+  screenProps: NavigationScreenProps
+}
+
+const ResultsList = ({
+  title,
+  results,
+  navigation,
+  screenProps,
+}: {
+  title: string
+  results: any
+  navigation?: NavigationStackScreenOptions | NavigationScreenProps
+  screenProps?: NavigationScreenProps
+}): NavigationStackScreenOptions | void | JSX.Element | any => {
+  // Navigate to details view
+  const buttonPress = (): NavigationStackScreenOptions | void | any => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore: unavailable type definition
+    navigation.navigate('ResultsShow')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <Text>Results: {results.length}</Text>
-      <FlatList<FlatListProps<any>>
+      <FlatList
         horizontal
         showsHorizontalScrollIndicator={false}
         data={results}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-        // @ts-ignore: unavailable type definition
-        keyExtractor={result => result.id}
+        keyExtractor={(result: any) => result.id}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />
+          return (
+            <TouchableOpacity onPress={buttonPress}>
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
@@ -41,4 +74,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default ResultsList
+export default withNavigation(ResultsList)
